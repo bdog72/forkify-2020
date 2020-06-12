ca24d0257245391fa6c31c94b23f30ed6aeac194
Brian Bycynski <high5bri@hotmail.com>
2020-06-12 03:17:38 -0400

    finished lecture 142

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,30 @@
 //
 //
 // Global app controller
+
+import Search from './models/Search';
+
+const state = {};
+
+const controlSearch = async () => {
+  // 1) Get query from view
+  const query = 'pizza'; // TODO
+
+  if (query) {
+    // 2) New search object and add to state
+    state.search = new Search(query);
+
+    // 3) Prepare UI for result
+
+    // 4) Search for recipes
+    await state.search.getResults();
+
+    // 5) Render results on UI
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector('.search').addEventListener('submit', (e) => {
+  e.preventDefault();
+  controlSearch();
+});
